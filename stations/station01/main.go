7289f4f13dd449f76289088d1d9114af633b4c77
mafyyu <morita0126m@gmail.com>
2025-09-15 14:14:40 +0900

    [Clear ]fizzbuzz

--- stations/station01/main.go
+++ stations/station01/main.go
@@ -9,3 +9,17 @@ func main() {
 
 	fmt.Println("FizzBuzz completed!")
 }
+
+func FizzBuzz(){
+	for i:=1;i<=100;i++{
+		if i%15==0 {
+			fmt.Println("FizzBuzz")
+		}else if i%5==0{
+			fmt.Println("Buzz")
+		}else if i%3==0{
+			fmt.Println("Fizz")
+		}else{
+			fmt.Println(i)
+		}
+	}
+}
